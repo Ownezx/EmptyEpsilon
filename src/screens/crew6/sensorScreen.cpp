a16a5abcff84ea14e321d24dc014366eddee46b0
Elliot Drees <elliot@trellick.net>
2025-06-14 23:35:22 -0400

    Created empty sensor screen

---
+++ src/screens/crew6/sensorScreen.cpp
@@ -0,0 +1,17 @@
+#include "sensorScreen.h"
+
+SensorScreen::SensorScreen(GuiContainer* owner, CrewPosition crew_position)
+: GuiOverlay(owner, "SCIENCE_SCREEN", colorConfig.background)
+{
+    
+}
+
+void SensorScreen::onDraw(sp::RenderTarget& renderer)
+{
+   
+}
+
+void SensorScreen::onUpdate()
+{
+   
+}
