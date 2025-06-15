c3272ebc7b1b083abefa09b461651ee25877358b
Elliot Drees <elliot@trellick.net>
2025-06-15 19:00:04 -0400

    WIP changing the backend for the scanners

--- src/screenComponents/rawScannerDataRadarOverlay.cpp
+++ src/screenComponents/rawScannerDataRadarOverlay.cpp
@@ -5,6 +5,7 @@
 #include "components/collision.h"
 #include "components/radar.h"
 #include "ecs/query.h"
+#include "utils/rawScannerUtil.h"
 
 
 RawScannerDataRadarOverlay::RawScannerDataRadarOverlay(GuiRadarView* owner, string id)
@@ -17,7 +18,6 @@ void RawScannerDataRadarOverlay::onDraw(sp::RenderTarget& renderer)
 {
     if (!my_spaceship)
         return;
-    auto distance = radar->getDistance();
 
     auto view_position = radar->getViewPosition();
     float view_rotation = radar->getViewRotation();
@@ -27,98 +27,13 @@ void RawScannerDataRadarOverlay::onDraw(sp::RenderTarget& renderer)
     const int point_count = 512;
     float radius = std::min(rect.size.x, rect.size.y) / 2.0f;
 
-    RawRadarSignatureInfo signatures[point_count];
-
-    // For each SpaceObject ...
-    for(auto [entity, signature, dynamic_signature, transform] : sp::ecs::Query<RawRadarSignatureInfo, sp::ecs::optional<DynamicRadarSignatureInfo>, sp::Transform>())
+    std::vector<float> angles(point_count);
+    for (int i = 0; i < point_count; i++)
     {
-        // Don't measure our own ship.
-        if (entity == my_spaceship)
-            continue;
-
-        // Initialize angle, distance, and scale variables.
-        float a_0, a_1;
-        float dist = glm::length(transform.getPosition() - view_position);
-        float scale = 1.0;
-
-        // If the object is more than twice as far away as the maximum radar
-        // range, disregard it.
-        if (dist > distance * 2.0f)
-            continue;
-
-        // The further away the object is, the less its effect on radar data.
-        if (dist > distance)
-            scale = 1.0f - ((dist - distance) / distance);
-
-        auto physics = entity.getComponent<sp::Physics>();
-        // If we're adjacent to the object ...
-        if (physics && dist <= physics->getSize().x)
-        {
-            // ... affect all angles of the radar.
-            a_0 = 0.0f;
-            a_1 = 360.0f;
-        }else{
-            // Otherwise, measure the affected range of angles by the object's
-            // distance and radius.
-            float a_diff = glm::degrees(asinf((physics ? physics->getSize().x : 300.0f) / dist));
-            float a_center = vec2ToAngle(transform.getPosition() - view_position);
-            a_0 = a_center - a_diff;
-            a_1 = a_center + a_diff;
-        }
-
-        // Get the object's radar signature.
-        // If the object is a SpaceShip, adjust the signature dynamically based
-        // on its current state and activity.
-        RawRadarSignatureInfo info = signature;
-        if (dynamic_signature)
-        {
-            info.gravity += dynamic_signature->gravity;
-            info.electrical += dynamic_signature->electrical;
-            info.biological += dynamic_signature->biological;
-        }
-
-        // For each interval determined by the level of raw data resolution,
-        // initialize the signatures array.
-        for(float a = a_0; a <= a_1; a += 360.f / float(point_count))
-        {
-            int idx = (int(a / 360.0f * point_count) + point_count * 2) % point_count;
-            signatures[idx] += info * scale;
-        }
+        angles[i] = 360.0F * float(i) / float(point_count);
     }
 
-    // Initialize the data's amplitude along each of the three color bands.
-    float amp_r[point_count];
-    float amp_g[point_count];
-    float amp_b[point_count];
-
-    // For each data point ...
-    for(int n = 0; n < point_count; n++)
-    {
-        // ... initialize its values in the array ...
-        signatures[n].gravity = std::max(0.0f, std::min(1.0f, signatures[n].gravity));
-        signatures[n].electrical = std::max(0.0f, std::min(1.0f, signatures[n].electrical));
-        signatures[n].biological = std::max(0.0f, std::min(1.0f, signatures[n].biological));
-
-        // ... make some noise ...
-        float r = random(-1, 1);
-        float g = random(-1, 1);
-        float b = random(-1, 1);
-
-        // ... and then modify the bands' values based on the object's signature.
-        // Biological signatures amplify the green band.
-        g += signatures[n].biological * 30;
-
-        // Electrical signatures amplify the red band.
-        r += random(-20, 20) * signatures[n].electrical;
-
-        // Gravitational signatures amplify the blue band.
-        b = b * (1.0f - signatures[n].gravity) + 40 * signatures[n].gravity;
-
-        // Apply the values to the radar bands.
-        amp_r[n] = r;
-        amp_g[n] = g;
-        amp_b[n] = b;
-    }
+    std::vector<RawScannerDataPoint> scanner_data = CalculateRawScannerData(view_position, angles, radius);
 
     // Create a vertex array containing each data point.
     std::vector<glm::vec2> a_r;
@@ -133,18 +48,12 @@ void RawScannerDataRadarOverlay::onDraw(sp::RenderTarget& renderer)
         float g = 0.0;
         float b = 0.0;
 
-        // ... then sum the amplitude values ...
-        for(int m = n - 2 + point_count; m <= n + 2 + point_count; m++)
-        {
-            r += amp_r[m % point_count];
-            g += amp_g[m % point_count];
-            b += amp_b[m % point_count];
-        }
+        printf("Scanner data %f, %f, %f, at %f\n", r, g, b, angles[n]);
 
         // ... divide them by 5 ...
-        r /= 5;
-        g /= 5;
-        b /= 5;
+        r = scanner_data[n].electrical / 5;
+        g = scanner_data[n].biological / 5;
+        b = scanner_data[n].gravity / 5;
 
         // ... and add vectors for each point.
         a_r.push_back(
