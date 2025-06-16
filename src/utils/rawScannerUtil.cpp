3210d11138fc8ec1b093e766b8a89c47bb98220f
Elliot Drees <elliot@trellick.net>
2025-06-16 18:31:46 -0400

    Interpolation for far object for them to be detected.

--- src/utils/rawScannerUtil.cpp
+++ src/utils/rawScannerUtil.cpp
@@ -4,6 +4,7 @@
 #include "playerInfo.h"
 #include "random.h"
 #include "components/collision.h"
+#include <cmath>
 
 #define NOISE_FLOOR 1
 
@@ -13,23 +14,49 @@ float sumFunction(float angle, float target_angle, float target_angle_width)
     return 1 - temp * temp ;
 }
 
+float farSumFunction(float angle, float target_angle, float target_angle_width, float resolution)
+{
+    // If the whole target width is within one resolution, we just return 1.
+    if (target_angle_width < resolution / 2 && abs(angle - target_angle) < resolution / 2)
+        return 1;
+    // If none of the target angle is within the resolution, we return 0.
+    if (abs(angle - target_angle) > resolution / 2 + target_angle_width / 2)
+        return 0;
+    // Otherwise, we return a linear interpolation between 0 and 1.
+    float p = 2 * abs(target_angle - angle) / (resolution  + target_angle_width );
+    return 1 - p;
+
+}
+
 // TODO: MAKE THIS USE A START ANGLE/END ANGLE AND RESOLUTION TO BE ABLE
 // TO HANDLE THE CASE WHERE THE ANGLE OF THE OBJECT IS SMALLER THAN THE RESOLUTION
-std::vector<RawScannerDataPoint> CalculateRawScannerData(glm::vec2 position, std::vector<float> angles, float range)
+std::vector<RawScannerDataPoint> CalculateRawScannerData(glm::vec2 position, float start_angle, float arc_size, uint point_count, float range)
 {
 
     // Initialize the data's amplitude along each of the three color bands.
-    std::vector<RawScannerDataPoint> return_data_points(angles.size());
+    std::vector<RawScannerDataPoint> return_data_points(point_count);
+
+    float resolution = arc_size / (point_count-1);
+
+    // Pre allocate the angles for computational speed
+    float angles[point_count];
+    for (int i = 0; i < point_count; i++)
+    {
+        // Here we do not want to wrap around as the sum function needs it to not wrap around.
+        angles[i] = start_angle + i * resolution;
+    }
+    
+    bool arc_loops_on_itself = false;
+    if (arc_size == 360 - resolution)
+        arc_loops_on_itself = true;
+
 
     // For each SpaceObject ...
     for (auto [entity, signature, dynamic_signature, transform] : sp::ecs::Query<RawRadarSignatureInfo, sp::ecs::optional<DynamicRadarSignatureInfo>, sp::Transform>())
     {
         // Don't measure our own ship.
         if (entity == my_spaceship)
-        {
-            printf("Skipping own ship\n");
             continue;
-        }
 
         // Initialize angle, distance, and scale variables.
         float dist = glm::length(transform.getPosition() - position);
@@ -37,10 +64,7 @@ std::vector<RawScannerDataPoint> CalculateRawScannerData(glm::vec2 position, std
         // If the object is further than the maximum range
         // ignore it
         if (dist > range)
-        {
-            printf("Skipping object at distance %f, further than range %f\n", dist, range);
             continue;
-        }
 
         float scale = 1.0;
         // The further away the object is, the less its effect on radar data.
@@ -70,23 +94,48 @@ std::vector<RawScannerDataPoint> CalculateRawScannerData(glm::vec2 position, std
             a_diff = glm::degrees(asinf((physics ? physics->getSize().x : 300.0f) / dist));
         }
 
-        // Now add the value to all relevant point
-        for (int i = 0; i < (int)angles.size(); i++)
-        {
-            if (abs(angles[i] - a_center) > abs(a_diff))
-                continue;
+        // Here we need to find where the angle starts to do the sum
+        int target_start_angle_index = (int) ceil(((a_center - a_diff) - start_angle) / resolution);
+        int target_stop_angle_index = (int) ceil(((a_center + a_diff) - start_angle) / resolution);
 
+        // Special case to use a different summing function if the object is very far
+        bool is_far = false;
+        if (target_stop_angle_index - target_start_angle_index <= 3)
+            is_far = true;
+
+        // Handle the cases where the the arc loops on itself.
+        if (!arc_loops_on_itself)
+        {
+            // If the start angle is negative, we need to wrap it around
+            if (target_start_angle_index < 0)
+            {
+                target_start_angle_index += point_count;
+                target_stop_angle_index += point_count;
+            }
+            // Prevent overflow
+            if (target_stop_angle_index >= point_count)
+                target_stop_angle_index = point_count - 1;
+        }
+
+        printf("Indexes: %d to %d\n", target_start_angle_index, target_stop_angle_index);
+        // Now add the value to all relevant point
+        for (int i = target_start_angle_index; i < target_stop_angle_index; i++)
+        {
+            printf("Adding to point %d, corresponding to angle %f\n", i, angles[i % point_count]);
             float summing_function_value = 0;
             if (p == 0)
             {
                 // If we do not intersect with the object we just use the sensor
                 // signal function 1 - (x/(2*a_diff)^2
-                summing_function_value = sumFunction(angles[i], a_center, a_diff);
+                if (is_far)
+                    summing_function_value = farSumFunction(angles[i % point_count], a_center, a_diff, resolution);
+                else
+                     summing_function_value = sumFunction(angles[i % point_count], a_center, a_diff);
             }
             else
             {
                 // If we intersect with it we do a quadratic interpolation with 1 depending on the closeness.
-                summing_function_value = sumFunction(angles[i], a_center, M_PI) * p + 1 + p;
+                summing_function_value = sumFunction(angles[i % point_count], a_center, M_PI) * p + 1 + p;
             }
 
             // Now we do the first sum for things
@@ -108,15 +157,10 @@ std::vector<RawScannerDataPoint> CalculateRawScannerData(glm::vec2 position, std
         }
     }
 
-    RawRadarSignatureInfo signatures[angles.size()];
-
     // For each data point ...
     // Now post processing to be as close to what it was before
-    for (int i = 0; i < (int)angles.size(); i++)
+    for (int i = 0; i < point_count; i++)
     {
-        //return_data_points[i].biological = std::clamp(return_data_points[i].biological, 0.0f, 1.0f);
-        //return_data_points[i].electrical = std::clamp(return_data_points[i].electrical, 0.0f, 1.0f);
-        //return_data_points[i].gravity = std::clamp(return_data_points[i].gravity, 0.0f, 1.0f);
 
         return_data_points[i].biological = random(0, NOISE_FLOOR) + return_data_points[i].biological * 40;
         return_data_points[i].electrical = random(0, NOISE_FLOOR) + random(0, 60) * return_data_points[i].electrical;
@@ -128,4 +172,9 @@ std::vector<RawScannerDataPoint> CalculateRawScannerData(glm::vec2 position, std
     }
 
     return return_data_points;
+}
+
+std::vector<RawScannerDataPoint> Calculate360RawScannerData(glm::vec2 position, uint point_count, float range)
+{
+    return CalculateRawScannerData(position, 0, 360 - 360 / float(point_count), point_count, range);
 }
